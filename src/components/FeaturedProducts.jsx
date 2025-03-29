318c769562762362ec23876b26efc208ae1146b7
Viral Vaghela <viralvaghela8577@gmail.com>
2025-03-29 17:50:15 +0530

    chore: update vite version to 6.2.3 and clean up FeaturedProducts component

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -15,7 +15,7 @@ const FeaturedProducts = () => {
         `${backendUrl}/api/get/featured-products`
       );
       setProducts(data.featuredProducts);
-      console.log(data)
+      // console.log(data)
     } catch (error) {
       console.log(error);
     }
@@ -45,11 +45,11 @@ const FeaturedProducts = () => {
           <div className="text-center mt-12">
             <a
               href="/view-all-products"
-              class="relative inline-block px-4 py-2 font-medium group"
+              className="relative inline-block px-4 py-2 font-medium group"
             >
-              <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-              <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-              <span class="relative text-black group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+              <span className="relative text-black group-hover:text-white">
                 VIEW ALL PRODUCTS
               </span>
             </a>
